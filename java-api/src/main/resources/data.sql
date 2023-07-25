a1afadde55a303c86198f6db948493cd82c82670
Akshay Pujare <akshaypujare7@gmail.com>
2023-07-25 19:29:33 +0100

    fixed some errors in schema

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO owners (owner_id, name) VALUES (1, 'Cilian');
 INSERT INTO owners (owner_id, name) VALUES (2, 'Jim');
 
-INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Jimmy', 3,1);
-INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Tom', 5),1;
-INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7,2);
+INSERT INTO dogs (dog_id, name, age,owner_id) VALUES (1, 'Jimmy', 3,1);
+INSERT INTO dogs (dog_id, name, age, owner_id) VALUES (2, 'Tom', 5,1);
+INSERT INTO dogs (dog_id, name, age, owner_id) VALUES (3, 'Lassie', 7,2);
--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS dogs;
-DROP TABLE IF EXISTS owners
+DROP TABLE IF EXISTS owners;
 
 CREATE TABLE owners (
     owner_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -11,5 +11,5 @@ CREATE TABLE dogs (
     name VARCHAR(250) NOT NULL,
     age INT NOT NULL,
     owner_id INT NOT NULL,
-    FOREIGN KEY (owner_id) REFERENCES owner(owner_id)
+    FOREIGN KEY (owner_id) REFERENCES owners(owner_id)
 );
