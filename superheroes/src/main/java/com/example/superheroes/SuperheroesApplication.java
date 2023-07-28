f8a3abd1611e31549649d16b1ba2cf097dd92bcc
Akshay Pujare <akshaypujare7@gmail.com>
2023-07-28 17:31:26 +0100

    QL-6 in progress

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -14,37 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-public class SuperheroesApplication implements CommandLineRunner {
-    @Autowired
-    HeroService heroService;
-
+public class SuperheroesApplication {
     public static void main(String[] args) {
         SpringApplication.run(SuperheroesApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        List<Hero> heroes = heroService.getHeroesStartingWithLetter("C");
-        List<Hero> allHeroes = heroService.getAllHeroes();
-        List<Hero> lastThreeHeroesByName = heroService.getLastThreeHeroesByName();
-        for (Hero hero: heroes){
-            System.out.println(hero);
-        }
-        for (Hero hero: allHeroes){
-            System.out.println(hero);
-        }
-        for (Hero hero: lastThreeHeroesByName){
-            System.out.println(hero);
-        }
-    }
-
-    @PostConstruct
-    public void init(){
-        System.out.println("Calculator has been created!");
-    }
-
-    @PreDestroy
-    public void destroy(){
-        System.out.println("Calculator will be destroyed");
     }
 }
