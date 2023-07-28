70366574e1eddd15d2f165178e0c67bd58099b58
Akshay Pujare <akshaypujare7@gmail.com>
2023-07-28 19:09:50 +0100

    QL6

--- superheroes/src/main/java/com/example/superheroes/controller/HeroController.java
+++ superheroes/src/main/java/com/example/superheroes/controller/HeroController.java
@@ -4,6 +4,7 @@ import com.example.superheroes.model.Hero;
 import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +17,10 @@ public class HeroController {
     @GetMapping("/heroes")
     public List<Hero> getAllHeroes(){
         return heroService.getAllHeroes();
+    }
+
+    @GetMapping("/heroes/{letter}")
+    public List<Hero> getHeroesStartingWithLetter(@PathVariable String letter){
+        return heroService.getHeroesStartingWithLetter(letter);
     }
 }
