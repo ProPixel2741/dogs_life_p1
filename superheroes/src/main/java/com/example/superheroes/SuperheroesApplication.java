e892682d496c168a272700ac3261afefc6cac34f
brion-gahl-side <brion.d.gahl@gmail.com>
2023-07-28 14:44:07 +0100

    QL4 and 5

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,14 +1,22 @@
 package com.example.superheroes;
 
+import com.example.superheroes.model.Hero;
 import com.example.superheroes.practice.Calculator;
+import com.example.superheroes.repository.HeroRepository;
+import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
 
+
+	@Autowired
+	HeroService heroService;
 	@Autowired
 	Calculator calc;
 
@@ -21,5 +29,9 @@ public class SuperheroesApplication implements CommandLineRunner {
 		System.out.println("Hello World!");
 		int result = calc.addition(3,4);
 		System.out.println(result);
+		List<Hero> heroes = heroService.getHeroesStartingWithLetter("C");
+		for (Hero hero: heroes) {
+			System.out.println(hero);
+		}
 	}
 }
---
+++ superheroes/src/main/java/com/example/superheroes/service/HeroService.java
@@ -0,0 +1,23 @@
+package com.example.superheroes.service;
+
+import com.example.superheroes.model.Hero;
+import com.example.superheroes.repository.HeroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HeroService {
+
+    @Autowired
+    private HeroRepository heroRepository;
+
+    public List<Hero> getAllHeroes() {
+        return heroRepository.findAll();
+    }
+    public List<Hero> getHeroesStartingWithLetter(String letter) {
+        String letterParam = letter + "%";
+        return heroRepository.findHeroesNameStartingWithLetter(letterParam);
+    }
+}
