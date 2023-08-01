b954f3281f792626217ddcdda659734ab8c34608
propixel2741 <propixel2741@gmail.com>
2023-08-01 14:39:29 +0100

    QL5 Completed - Bjorn

--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import batman from '../images/batman.jpg';
 import images from '../index.js';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col'; // Import Col from react-bootstrap
+import Row from 'react-bootstrap/Row';
 const HeroDetail = (props) => {
-    return (
-      <Card >
-        <div className="imgContainer">
-          <Card.Img variant="top" src={images[props.info.alias.replaceAll(/\s/g, '')]} width="150px" />
-        </div>
-        <Card.Body >
-          <Card.Title >Alias: {props.info.alias}</Card.Title>
-          <Card.Text >Name: {props.info.name}</Card.Text>
-          <Button variant="primary">Like</Button>
-        </Card.Body>
-      </Card>
-    )
+  const[counter, setCounter] = useState(0);
+  const incrementCounter =()=>{
+    setCounter(counter+1);
   }
-  
+
+  return (
+    <Card>
+      <div className="imgContainer">
+        <Card.Img variant="top" src={images[props.info.alias.replaceAll(/\s/g, '')]} width="150px" />
+      </div>
+      <Card.Body>
+        <Card.Title>Alias: {props.info.alias}</Card.Title>
+        <Card.Text>Name: {props.info.name}</Card.Text>
+        <Row>
+          <Col>
+            <Button variant="primary" onClick={incrementCounter}>Like</Button>
+          </Col>
+          <Col>
+            {/* Use the shorthand notation for conditional rendering */}
+            {counter > 0 && <Card.Text>Likes: {counter}</Card.Text>}
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+  }
+
 export default HeroDetail
